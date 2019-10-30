3a26c85ae42af8e98f435af2a9c914bce58077d7
mochibot <amusaur83@gmail.com>
2019-10-30 13:51:18 -0700

    day1 MVP completed: added move room parser

--- src/adv.py
+++ src/adv.py
@@ -5,21 +5,19 @@ from player import Player
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     """North of you, the cave mount beckons"""),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("Foyer", 
+                     """Dim light filters in from the south. Dusty passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook", 
+                     """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage", 
+                     """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room("Treasure Chamber", 
+                     """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
 }
 
 
@@ -42,9 +40,6 @@ room['treasure'].s_to = room['narrow']
 
 player1 = Player('Player1', room['outside'])
 
-print(player1.name)
-print(player1.current_room.name)
-
 # Write a loop that:
 #
 # * Prints the current room name
@@ -55,3 +50,18 @@ print(player1.current_room.name)
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+while True:
+    print(f'You are currently in: {player1.current_room.name}')
+    print(player1.current_room.description)
+    print('Commands: n -- move north | s -- move south | e -- move east | w -- move west | q -- quit')
+    input_direction = input('Enter the direction to move to: ')
+
+    if input_direction == 'q':
+        print('Goodbye')
+        break
+    elif input_direction == 'n' or input_direction == 'w' or input_direction =='e' or input_direction == 's':
+        player1.move_room(input_direction)
+    else:
+        print('Please select a valid option (n, s, e, w, q).')
+        continue
--- src/player.py
+++ src/player.py
@@ -5,3 +5,14 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+
+    def move_room(self, direction):
+        moves = {'n': self.current_room.n_to,
+                 's': self.current_room.s_to,
+                 'e': self.current_room.e_to,
+                 'w': self.current_room.w_to}
+
+        if moves[direction] != None:
+            self.current_room = moves[direction]
+        else:
+            print('Move is not allowed')
